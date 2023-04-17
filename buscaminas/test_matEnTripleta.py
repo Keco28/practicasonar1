d24b83eee2d6d2c862bf59044a4500ddfd66b707
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-04-17 15:45:12 -0500

    Cambios para test

---
+++ buscaminas/test_matEnTripleta.py
@@ -0,0 +1,10 @@
+import matEnTripletas
+import unittest
+
+class testBuscaminas(unittest.TestCase):
+    
+    def tripleta_test(self):
+        self.assertIs(list[0])
+
+if __name__ == "__matEnTripletas":
+    unittest.main()
